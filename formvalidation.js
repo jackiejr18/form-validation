8e9fcc499abff6658318edf6c4e3c079eb44f88f
Jackie Jr <jwysinger@gmail.com>
2023-10-07 10:00:54 -0500

    javascript file with with defining of variables for each error message

---
+++ formvalidation.js
@@ -0,0 +1,5 @@
+var nameError = document.getElementById('name-error');
+var phoneError = document.getElementById('phone-error');
+var emailError = document.getElementById('email-error');
+var messegeError = document.getElementById('message-error');
+var submitError = document.getElementById('subit-error');
