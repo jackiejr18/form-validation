06c6901a9e2336d8f27c7bee4099613c949d7a06
Jackie Jr <jwysinger@gmail.com>
2023-10-09 10:02:44 -0500

    validateEmail function now works for email input

--- formvalidation.js
+++ formvalidation.js
@@ -38,3 +38,19 @@ function validatePhone() {
   phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
   return true;  
 }
+
+function validateEmail() {
+  var email = document.getElementById('contact-email').value;
+  
+  if(email.length == 0) {
+    emailError.innerHTML = 'Email is required';
+    return false;
+  }
+  if(!email.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    emailError.innerHTML = 'Email Invalid';
+    return false;
+  }
+
+  emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
